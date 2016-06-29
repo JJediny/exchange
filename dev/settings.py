381cc7ba6988af1543f60f01dbfa543fc12fe3e8
Darin <darinjacosta@gmail.com>
2016-06-29 10:26:59 -0500

    add reverse proxy

--- dev/settings.py
+++ dev/settings.py
@@ -52,7 +52,7 @@ DATABASES = {
     }
 }
 
-GEOSERVER_URL = 'http://192.168.99.110:8888/proxy/http://192.168.99.110:8080/geoserver/'
+GEOSERVER_URL = 'http://192.168.99.110:8080/geoserver/'
 DEFAULT_MAP_CRS = "EPSG:4326"
 
 OGC_SERVER = {
